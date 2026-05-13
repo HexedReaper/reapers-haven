2f9504e1051027a4f12fd1b9842a33e3fa7b5afd
HexedReaper <281075380+HexedReaper@users.noreply.github.com>
2026-05-13 15:37:14 +0200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://LordAcid.codeberg.page',
+  base: '/reapers-haven',
+  outDir: './dist',
+});
