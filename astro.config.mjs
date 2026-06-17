70db8253967a4d68d709efe11328fc7f11454a90
HexedReaper <281075380+HexedReaper@users.noreply.github.com>
2026-06-17 22:19:34 +0200

    fix all links, fix styles for index.css, fix links for sidebars.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
   site: 'https://reapers-haven.pages.dev',
   base: '/',
   outDir: './dist',
+  vite: {
+    plugins: [viteSingleFile()]
+  }
 });
